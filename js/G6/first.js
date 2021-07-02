dc365e8461f9c2a1ac930f5ce316b00165236f1e
zhoujianxiang <453139846@qq.com>
2021-07-02 17:27:16 +0800

    antv大数据图表demo

--- js/G6/first.js
+++ js/G6/first.js
@@ -3,7 +3,7 @@
  * @author: zhoujianxiang
  * @Date: 2021-06-18 15:08:51
  * @LastEditors: zhoujianxiang
- * @LastEditTime: 2021-06-18 15:30:15
+ * @LastEditTime: 2021-07-02 17:16:27
  * @Copyright: 2020 cheworld.com Inc. All rights reserved. 版权所有
  * 深圳市万普合信科技有限公司（91440300MA5FUB0P88） 注意：仅限于公司内部传阅，禁止外泄以及用于其他的商业目的
  */
@@ -25,7 +25,8 @@ const data = {
   edges: [
     {
       source: "node1",
-      target: "node2"
+      target: "node2",
+      label: '我是连线', // 边的文本
     }
   ]
 };
@@ -33,14 +34,38 @@ const data = {
 const graph = new G6.Graph({
   container: "container",
   width: 1000,
-  height: 500,
+  height: 1000,
+  fitView: true, // 图表自适应画布大小
+  fitViewPadding: [20, 40, 50, 20], 
+  animate: true,
+  modes: {
+    // 支持的 behavior
+    default: ['drag-canvas', 'zoom-canvas','click-select'],
+    edit: ['click-select'],
+  },
+  nodeStateStyles:{
+    hover:{
+      style: {
+        fill: "yellow",
+        lineWidth: 3
+      },
+    },
+    click: {
+      labelCfg: {
+        style: {
+          fill: "#333",
+          fontSize: 20
+        }
+      }
+    }
+  },
   defaultNode: {
     shape: "ellipse",
     //type: 'rect',
     size: [100],
     color: "#5B8FF9",
     style: {
-      fill: "red",
+      fill: "#ccc",
       lineWidth: 3
     },
     labelCfg: {
@@ -52,10 +77,51 @@ const graph = new G6.Graph({
   },
   defaultEdge: {
     style: {
-      stroke: "red"
+      stroke: "#000",
+      opacity: 0.2
     }
   }
 });
 
-graph.data(data);
-graph.render();
+const main = async () => {
+  // const response = await fetch(
+  //   'http://127.0.0.1:8081/data.json',
+  // );
+  const remoteData = dataJson;
+
+  // ...
+  console.log(remoteData)
+  setNodeStyle(remoteData)
+  graph.data(remoteData); // 加载远程数据
+  graph.render(); // 渲染
+};
+
+function setNodeStyle(data){
+  const nodes = data.nodes;
+  nodes.forEach((node) => {
+    if (!node.style) {
+      node.style = {};
+    }
+    switch (
+      node.class // 根据节点数据中的 class 属性配置图形
+    ) {
+      case 'c0': {
+        node.type = 'circle'; // class = 'c0' 时节点图形为 circle
+        break;
+      }
+      case 'c1': {
+        node.type = 'rect'; // class = 'c1' 时节点图形为 rect
+        node.size = [35, 20]; // class = 'c1' 时节点大小
+        break;
+      }
+      case 'c2': {
+        node.type = 'ellipse'; // class = 'c2' 时节点图形为 ellipse
+        node.size = [35, 20]; // class = 'c2' 时节点大小
+        break;
+      }
+    }
+  });
+  return data;
+}
+
+main()
