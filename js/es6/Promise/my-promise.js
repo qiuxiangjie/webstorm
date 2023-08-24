6f8d90bf8ebd5d49c7ca36ce3c6ab6aa4d86ec5b
zhoujianxiang <453139846@qq.com>
2023-08-24 10:58:55 +0800

    隐式转换

--- js/es6/Promise/my-promise.js
+++ js/es6/Promise/my-promise.js
@@ -1,12 +1,4 @@
-/*
- * @Descripttion: 
- * @author: zhoujianxiang
- * @Date: 2022-05-23 11:24:23
- * @LastEditors: zhoujianxiang
- * @LastEditTime: 2022-05-24 10:00:33
- * @Copyright: 2020 cheworld.com Inc. All rights reserved. 版权所有
- * 深圳市万普合信科技有限公司（91440300MA5FUB0P88） 注意：仅限于公司内部传阅，禁止外泄以及用于其他的商业目的
- */
+
 // function MyPromise(fn){
 //   this.fns = []
 //   this.state = 'pending'
